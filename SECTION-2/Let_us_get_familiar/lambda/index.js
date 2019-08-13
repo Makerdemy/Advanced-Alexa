ed2312cbf87c423458f7a370ee54d303dd134dbb
Makerdemy <36653765+Makerdemy@users.noreply.github.com>
2019-08-13 10:59:08 +0530

    Update index.js

--- SECTION-2/Let_us_get_familiar/lambda/index.js
+++ SECTION-2/Let_us_get_familiar/lambda/index.js
@@ -1,14 +1,17 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
-const Alexa = require('ask-sdk-core');
-var username;
-var job;
-var place;
-var experience;
-var sport;
-var hobby;
+// Makerdemy let us get familiar skill 
+// This code sample demonstrates handling intents using Alexa Skills Kit SDK (v2).
+// This is the beckend logic for let us get familiar
+// Take the ask sdk core version package
 
+const Alexa = require('ask-sdk-core');
+var username;   //global variable to store the user name
+var job;        //global variable to store the user's job
+var place;      //global variable to store the user's work place
+var experience; //global variable to store the user's working experience
+var sport;      //global variable to store the user's favorite sport
+var hobby;      //global variable to store the user's hobbies
+
+//Handler for handling the launchrequest
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
@@ -16,97 +19,140 @@ const LaunchRequestHandler = {
     handle(handlerInput) {
         const speechText = "Hi there! I'm Alexa. What's your name? ";
         const repromptText = "Oh Oh! I'm Alexa. May I know your name please";
+        
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(repromptText)
             .getResponse();
     }
 };
+
+//Handler for handling the NameIntent, through which the user name is known
 const NameIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'NameIntent';
     },
     handle(handlerInput) {
+        //store the user name using the "name" slot
         username = handlerInput.requestEnvelope.request.intent.slots.name.value;
-        const speechText = "Nice meeting you "+username+ "!. Can I know more about your, like what are you doing , studying or doing any job";
+        const speechText = `Nice meeting you ${username}!. Can I know more about your, 
+        like what are you doing , studying or doing any job`;
+        
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt('Hai ' +username+ 'Tell me more about yourself!' )
+            .reprompt('Hai ' +username+ 'Tell me what do you do for living!' )
             .getResponse();
     }
 };
+
+//Handler for handling the WorkIntent, this gives us the user's job
 const WorkIntentHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'WorkIntent';
+        return handlerInput.requestEnvelope.
+        request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.
+            request.intent.name === 'WorkIntent';
     },
     handle(handlerInput) {
-      if(handlerInput.requestEnvelope.request.intent.slots.job.value && !handlerInput.requestEnvelope.request.intent.slots.place.value && !handlerInput.requestEnvelope.request.intent.slots.year.value )
+      //if only job slot value is given by the user,this condition will be used
+      if(handlerInput.requestEnvelope.request.intent.slots.job.value &&
+      !handlerInput.requestEnvelope.request.intent.slots.place.value &&
+      !handlerInput.requestEnvelope.request.intent.slots.year.value )
        {
+        //store the user's job using the "job" slot  
         job = handlerInput.requestEnvelope.request.intent.slots.job.value;
-        const speechText = "Great "+username+ "!. Hope you are enjoying your role as a " +job+".Tell me which sport do you like or play more";
+        const speechText = `Great ${username}!. Hope you are enjoying your role as a ${job}.
+        Tell me which sport do you like or play more`;
+        
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt('Hai ' +username+ 'Tell me more about yourself!' )
+            .reprompt('Hai ' +username+ 'Tell me what is your favorite sport' )
             .getResponse();
        }
     
-     else if(handlerInput.requestEnvelope.request.intent.slots.job.value && handlerInput.requestEnvelope.request.intent.slots.place.value && !handlerInput.requestEnvelope.request.intent.slots.year.value )
+      //if job slot value and work place slot values are given by the user,this condition will be used
+     else if(handlerInput.requestEnvelope.request.intent.slots.job.value && 
+     handlerInput.requestEnvelope.request.intent.slots.place.value && 
+     !handlerInput.requestEnvelope.request.intent.slots.year.value )
      {
+         //store the user's job using the "job" slot 
          job = handlerInput.requestEnvelope.request.intent.slots.job.value;
+         
+         //store the user's work place using the "place" slot 
          place = handlerInput.requestEnvelope.request.intent.slots.place.value;
-        const speechText = "Great "+username+ "!.Hopeyou are doing well in your "+job+" in "+place+" . which sport do you like or play more";
+        const speechText = `Great ${username}!.Hopeyou are doing well in your ${job} in ${place}.
+        which sport do you like or play more`;
+        
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt('Hai ' +username+ 'Tell me more about yourself!' )
+            .reprompt('Hai ' +username+ 'Tell me what is your favorite sport' )
             .getResponse();
          
      }
-     else{
      
+     //if job slot value, work place slot,work experience values are given by the user,this condition will be used 
+     else{
+         //store the user's job using the "job" slot 
          job = handlerInput.requestEnvelope.request.intent.slots.job.value;
+         
+         //store the user's job or work place using the "place" slot 
          place = handlerInput.requestEnvelope.request.intent.slots.place.value;
+         
+         //store the user's exprerience in that job using the "expereience" slot 
          experience = handlerInput.requestEnvelope.request.intent.slots.year.value;
-        const speechText = "Thats so cool "+username+ "!.You are a "+job+" in "+place+" from "+experience+ " years That sounds great . which sport do you like or play more";
+         
+        const speechText = `Thats so cool ${username}!.You are a ${job} in ${place} from ${experience} years 
+        That sounds great . which sport do you like or play more`;
+        
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt('Hai ' +username+ 'Tell me more about yourself!' )
+            .reprompt('Hai ' +username+ 'Tell me what is your favorite sport' )
             .getResponse();
          
      }
     }  
     
 };
+
+//Handler for handling the Favoritesportshandler, this gives us the user's favorite sport
 const FavoriteSportIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'FavoriteSportIntent';
     },
     handle(handlerInput) {
+       //store the user's favorite sport using the "sport" slot 
        sport = handlerInput.requestEnvelope.request.intent.slots.sport.value;
-        const speechText = "Thats on point! I too like " +sport+ " mate. Hey " +username+" ,tell me what are your hobbies";
+        const speechText = `Thats on point! I too like ${sport} mate. Hey ${username} ,
+        tell me what are your hobbies`;
+        
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt('Hai ' +username+ 'Tell me more about yourself!' )
+            .reprompt('Hai ' +username+ 'Tell me what are your hobbies' )
             .getResponse();
     }
 };
 
+//Handler for handling the HobbyIntentHandler, this gives us the user's hobbies
 const HobbyIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'HobbyIntent';
     },
     handle(handlerInput) {
+       //store the user's hobbies using the "hobby" slot 
        hobby = handlerInput.requestEnvelope.request.intent.slots.hobby.value;
-        const speechText = "Your hobbies are looking cool,Thanks " +username+ " It is nice talking with you, See you later ";
+        const speechText = `Your hobbies are looking cool,Thanks ${username}+
+        It is nice talking with you, See you later`;
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt('Hai ' +username+ 'Tell me more about yourself!' )
+            .reprompt('Hey ' +username+ '.You do ' +hobby+ ' Cool!. Nice talking to you, see you' )
             .getResponse();
     }
 };
+
+//Handler for helpIntent,if the user asks for help regarding this skill
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -121,6 +167,9 @@ const HelpIntentHandler = {
             .getResponse();
     }
 };
+
+
+//Handler for Cancel Intent or Stop Intent,if the user says exits, stop or cancel 
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -134,20 +183,20 @@ const CancelAndStopIntentHandler = {
             .getResponse();
     }
 };
+
+//Handler for handling the Session Ended Intent, when the session ends
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
     },
     handle(handlerInput) {
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse();
+    return handlerInput.responseBuilder.getResponse();
     }
 };
 
 // The intent reflector is used for interaction model testing and debugging.
-// It will simply repeat the intent the user said. You can create custom handlers
-// for your intents by defining them above, then also adding them to the request
-// handler chain below.
+// It will return the intent that is triggered for a utterance from the user
+
 const IntentReflectorHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest';
@@ -158,7 +207,6 @@ const IntentReflectorHandler = {
 
         return handlerInput.responseBuilder
             .speak(speechText)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
@@ -187,10 +235,10 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        NameIntentHandler,
-        WorkIntentHandler,
-        FavoriteSportIntentHandler,
-        HobbyIntentHandler,
+        NameIntentHandler,    //To store name
+        WorkIntentHandler,    //To know and store the user's working information
+        FavoriteSportIntentHandler, //To know and store the user's favorite sport
+        HobbyIntentHandler,   //To know the and store the user's hobbies
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
